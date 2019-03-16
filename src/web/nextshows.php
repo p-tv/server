e75ebc6d39b6f04fbceb1dab21ff25e960837844
p-tv <p-tv@jmorrison.org>
2019-03-16 12:15:49 +1000

    Get api server from DI container prior to use.

--- src/web/nextshows.php
+++ src/web/nextshows.php
@@ -3,6 +3,8 @@
 require __DIR__ . '/../bootstrap.php';
 
 
+$apiService = $container->get(\ptv\service\APIService::class);
+
 $channelId = (int) $_GET['channelId'];
 $data = json_encode($apiService->getNextPrograms($channelId), JSON_UNESCAPED_SLASHES);
 echo $data;
